d77bc1555072465476a7b85dc9dcb31b96ec4809
Rohan B <rohanbhide19@hotmail.com>
2019-08-31 18:14:29 +0530

    add project files

---
+++ app/src/main/java/com/heliumfarticle/memo51/EditActivity.java
@@ -0,0 +1,166 @@
+package com.heliumfarticle.memo51;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashSet;
+
+public class EditActivity extends AppCompatActivity {
+
+    int noteId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        final EditText editText = findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+        noteId = intent.getIntExtra("noteId", -1);
+
+        if(noteId != -1){
+            editText.setText(MainActivity.notes.get(noteId));
+        }else{
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() - 1;
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.notes.set(noteId, String.valueOf(s));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.heliumfarticle.memo51", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        String string = MainActivity.notes.get(noteId);
+        String[] lines = string.split(System.getProperty("line.separator"));
+
+        setAlarm(lines);
+        calcTotal();
+
+    }
+
+    protected void calcTotal(){
+        int total = 0;
+        String string = MainActivity.notes.get(noteId);
+        if(!string.contains("@Rs("))
+            return;
+
+        while (true) {
+            String  answer = findSpecials("@Rs(", 4, -1);
+            if(answer.equals("n"))
+                break;
+            else {
+                total += Integer.valueOf(answer);
+                Toast.makeText(EditActivity.this, String.valueOf(total), Toast.LENGTH_SHORT).show();
+            }
+        }
+        string = MainActivity.notes.get(noteId);
+        MainActivity.notes.set(noteId, string.concat("\nTotal : " + total));
+        MainActivity.arrayAdapter.notifyDataSetChanged();
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.heliumfarticle.memo51", Context.MODE_PRIVATE);
+        HashSet<String> set = new HashSet<>(MainActivity.notes);
+        sharedPreferences.edit().putStringSet("notes", set).apply();
+    }
+
+    //not working for multiple alarms
+    //need to add intent array
+    protected void setAlarm(String[] lines){
+        while (true) {
+            String answer = findSpecials("@Alarm", 7, 12);
+            String[] parts = answer.split("\\.");
+            if(answer.equals("n")) {
+                break;
+            }
+            else {
+                int hour = Integer.valueOf(parts[0]);
+                int minute = Integer.valueOf(parts[1]);
+                String message = "Alarm by Memo 51";
+                for(int i = 0; i < lines.length; i++) {
+                    if (lines[i].contains("@Alarm(")) {
+                        if (i - 1 >= 0 && !(lines[i - 1].contains("@Alarm("))) {
+                            message = lines[i - 1];
+                        }
+                    }
+                }
+                Toast.makeText(EditActivity.this, hour + "+" + minute + "+" + message, Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(AlarmClock.ACTION_SET_ALARM);
+                i.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+                i.putExtra(AlarmClock.EXTRA_HOUR, hour);
+                i.putExtra(AlarmClock.EXTRA_MINUTES, minute);
+                i.putExtra(AlarmClock.EXTRA_MESSAGE, message);
+                startActivity(i);
+            }
+        }
+    }
+
+    protected String findSpecials(String special, int lengthSpecial, int syntaxLength){
+        String string = MainActivity.notes.get(noteId);
+        int startPos = string.indexOf(special);
+        int endPos = string.indexOf(")");
+        if(startPos == -1 || endPos == -1){
+            return "n";
+        }
+        String answer;
+
+        //for answer = till ")"
+        if(syntaxLength == -1){
+            answer = string.substring(startPos + lengthSpecial, endPos);
+            MainActivity.notes.set(noteId, string.replaceFirst("@", "added").replaceFirst("\\(", " . ").replaceFirst("\\)", "!"));
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+
+            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.heliumfarticle.memo51", Context.MODE_PRIVATE);
+            HashSet<String> set = new HashSet<>(MainActivity.notes);
+            sharedPreferences.edit().putStringSet("notes", set).apply();
+
+        }
+        else if(startPos + syntaxLength == endPos){
+            answer = string.substring(startPos + lengthSpecial, endPos);
+            MainActivity.notes.set(noteId, string.replaceFirst("@", "set").replaceFirst("\\(", " : ").replaceFirst("\\)", "!"));
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+
+            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.heliumfarticle.memo51", Context.MODE_PRIVATE);
+            HashSet<String> set = new HashSet<>(MainActivity.notes);
+            sharedPreferences.edit().putStringSet("notes", set).apply();
+            //Toast.makeText(EditActivity.this, "Found : " + answer, Toast.LENGTH_SHORT).show();
+        }else{
+            return "n";
+        }
+
+        return answer;
+    }
+
+}
---
+++ app/src/main/java/com/heliumfarticle/memo51/MainActivity.java
@@ -0,0 +1,122 @@
+package com.heliumfarticle.memo51;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EditActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ListView listView = findViewById(R.id.listView);
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.heliumfarticle.memo51", Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>)sharedPreferences.getStringSet("notes", null);
+        if(set == null) {
+
+            notes.add("Take A note!");
+
+        }else{
+            notes = new ArrayList<>(set);
+        }
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), EditActivity.class);
+                intent.putExtra("noteId", position);
+                startActivity(intent);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_delete)
+                        .setTitle("Confirm deletion")
+                        .setMessage("Delete this note?")
+                        .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                notes.remove(position);
+                                arrayAdapter.notifyDataSetChanged();
+
+                                if(notes.size() == 0){
+                                    notes.add("Take A Note!");
+                                }
+
+                                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.heliumfarticle.memo51", Context.MODE_PRIVATE);
+                                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .show();
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
